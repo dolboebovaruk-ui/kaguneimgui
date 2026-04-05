524bdc37d7f5881bdb43e835fa04f7758ae0904a
dolboebovaruk-ui <dolboebovaruk@gmail.com>
2026-04-05 14:49:03 +0200

    Create core module for kaguneimgui

---
+++ kaguneimgui/core.lua
@@ -0,0 +1,17 @@
+--[[
+KaguneImGui: A Comprehensive Lua UI Library
+Copyright (C) 2023 Dolboebovaruk UI
+This library provides a set of core modules, widgets, themes, and examples for building UIs in Lua.
+]]
+
+local kaguneimgui = {}
+
+function kaguneimgui.init()
+    -- Initialization code here
+end
+
+function kaguneimgui.run()
+    -- Main loop code here
+end
+
+return kaguneimgui
